d7e47bbc51acab9f770ed703472eb16a7758af47
Nicholas <n.ramkhal@gmail.com>
2022-01-27 22:51:32 -0500

    Added support for multiple simoultaneous electrons

--- MCElectronSim.m
+++ MCElectronSim.m
@@ -1,60 +1,64 @@
-close all;
-clear;
-set(0,'DefaultFigureWindowStyle','docked')    
-set(0,'DefaultLineLineWidth', 2);
+function [] = MCElectronSim(numElectrons)
+    close all;    
+    set(0,'DefaultFigureWindowStyle','docked')    
+    set(0,'DefaultLineLineWidth', 2);
 
-numSteps=500;
-t=0;
-x=0;  
-V=0;  
-Vavg=0;
+    numSteps=150;
 
-%(this runs verrrry slow)    
-%m = 9.10938215e-31; % electron mass 
-%q = 1.60217653e-19; % electron charge
-%F=q*E;              % force on an electron due to an electric field   
+    t=0;
+    x=zeros(numElectrons,1);  
+    V=zeros(numElectrons,1);  
+    Vavg=0;
 
-dt = 1; % Time increment
-afterCoeff=0; % Velocity after scattering
-%afterCoeff=-0.25;
-F=1;      % Force
-m = 1;    % mass 
+    %(this runs verrrry slow)    
+    %m = 9.10938215e-31; % electron mass 
+    %q = 1.60217653e-19; % electron charge
+    %F=q*E;              % force on an electron due to an electric field   
 
-for k=2:numSteps    
-    % Increment time
-    t(k)=t(k-1)+dt;
+    dt = 1; % Time increment
+    afterCoeff=0; % Velocity after scattering
+    %afterCoeff=-0.25;
+    F=1;      % Force
+    m = 1;    % mass 
 
-    % Velocity increases due to force exerted by an electric field        
-    V(k) = V(k-1) + F/m * dt;
+    for k=2:numSteps    
+        % Increment time
+        t(k)=t(k-1)+dt;
 
-    % Update average velocity
-    Vavg(k)=mean(V);
+        % Velocity increases due to force exerted by an electric field        
+        V(:,k) = V(:,k-1) + F/m * dt;
 
-    % Position increases by the velocity times time step    
-    x(k) = x(k-1) + V(k) * dt + F/m+dt^2/2;
+        % Update average velocity
+        Vavg(k)=mean(mean(V,2));
 
-    % Chance of scattering, which drops velocity to zero or reflects the
-    % electron
-    if rand()<=0.05
-        V(k)=V(k)*afterCoeff;
+        % Position increases by the velocity times time step    
+        x(:,k) = x(:,k-1) + V(:,k) * dt + F/m+dt^2/2;
+
+        % Chance of scattering, which drops velocity to zero or reflects the
+        % electron
+        mask = rand(numElectrons,1)<=0.05;
+        V(mask,k)=V(mask,k)*afterCoeff;   
+
+        for j=1:numElectrons
+            subplot(3,1,1);
+            plot(t,V(j,:),"b"); hold on;       
+            plot(t,Vavg,"go");
+            title("Drift Velocity: " + Vavg(k));
+            xlabel("time");
+            ylabel("velocity");
+
+
+            subplot(3,1,2);
+            plot(x(j,:),V(j,:),"g");hold on; 
+            xlabel("position");
+            ylabel("velocity");
+
+            subplot(3,1,3);
+            plot(t,x(j,:),"r"); hold on;  
+            xlabel("time");
+            ylabel("position");
+        end
+
+        pause(0.00001)
     end
-
-    subplot(3,1,1);
-    plot(t,V,"b"); hold on;       
-    plot(t,Vavg,"go"); hold off;
-    title("Drift Velocity: " + Vavg(k));
-    xlabel("time");
-    ylabel("velocity");
-
-    subplot(3,1,2);
-    plot(x,V,"g"); 
-    xlabel("position");
-    ylabel("velocity");
-
-    subplot(3,1,3);
-    plot(t,x,"r");   
-    xlabel("time");
-    ylabel("position");
-
-    pause(0.00001)
 end
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# ELEC 4700 MonteCarlo PA Assignment
+# ELEC 4700 Monte Carlo PA Assignment
 
-TODO: Multiple electrons simoultaneously
+1D particle (electron) acceleration simulation
+
+![Simulation with two electrons](./two_e.gif)
+
+## Usage
+
+On the console run `MCElectronSim(1)`. The parameter is the number of electrons to simulate. Note that the script slows down considerably with each additional electron ¯\\_(ツ)_/¯ 
